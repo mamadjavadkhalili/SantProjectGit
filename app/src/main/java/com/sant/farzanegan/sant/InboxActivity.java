2c9e07625e62d68871a6b8afd98b33e0fb62c55a
mamadjavadkhalili <mamadjavadkhalili@yahoo.com>
2017-08-16 13:15:27 +0430

    Initial commit

---
+++ app/src/main/java/com/sant/farzanegan/sant/InboxActivity.java
@@ -0,0 +1,16 @@
+package com.sant.farzanegan.sant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Farzanegan on 14/08/2017.
+ */
+public class InboxActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.inbox);
+  }
+}
---
+++ app/src/main/java/com/sant/farzanegan/sant/NeedForSegmentActivity.java
@@ -0,0 +1,13 @@
+package com.sant.farzanegan.sant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NeedForSegmentActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.forward);
+  }
+}
